91c58e8cf74db12c51e1f9a4b058d9fe87e79a45
Rafael Figueroa Gomez <rafaelrfg05@gmail.com>
2026-08-16 23:46:51 -0700

    Add gravity physics to bird

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ int main() {
     bird.setPosition({100.f, 300.f}); //Places the bird at the coordinates (100,300) in the window.
     //Also important to remember that positive Y means down and Positive X means right in SFML's coordinate system.
 
+    //The following lines will handle the physics of the bird's movement, including gravity and velocity
+    sf::Clock clock; //Tracks the time elapsed between frames, which is important for calculating the bird's movement based on time rather than frame rate. 
+    float velocity = 0.f; //The bird's vertical velocity, in pixels per second. Initial condition is 0 (Bird is not moving yet) and lives outside the loop to initialize it only
+    const float gravity = 500.f; //The acceleration due to gravity, in pixels per second squared
+
+
 
     while (window.isOpen()) {
         while(const std::optional<sf::Event> event = window.pollEvent()) {
@@ -16,6 +22,11 @@ int main() {
                 window.close();
             }
         }
+        float deltaTime = clock.restart().asSeconds(); //Calculates the time elapsed since the last frame(loop), which is used to update the bird's position based on its velocity
+        velocity += gravity * deltaTime; //Updates the bird's vertical velocity based on the acceleration due to gravity
+        bird.move({0.f, velocity * deltaTime}); //Updates the bird's position based on its velocity and the time elapsed since the last frame. 
+
+        
         window.clear(); //Clears the window to a black color for redrawing (re-rendering) the next frame.
         window.draw(bird); //Draws the bird sprite to the window.
         window.display(); //Displays the contents of the window to the screen. This is the last step in the rendering process for each frame. 
